6b40d9ef75e37f5418a5f3be7bd0642d22d9ba84
Ahmed Askar <ahmed.askar@vodafone.com>
2019-04-01 12:13:32 +0200

    test reset hard 1

--- Images-MVVM/AppDelegate.swift
+++ Images-MVVM/AppDelegate.swift
@@ -12,7 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    //test reset hard 1
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
