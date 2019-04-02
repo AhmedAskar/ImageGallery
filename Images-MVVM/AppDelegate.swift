aec897a2e5dac7b8d6e6b8d8fd17d57a0eb375ae
Ahmed Askar <ahmed.askar@vodafone.com>
2019-04-02 11:19:51 +0200

    cherry pick this commit

--- Images-MVVM/AppDelegate.swift
+++ Images-MVVM/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //test reset hard 4
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
